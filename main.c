303287722c5166a1aa941618ba2e0695b269f3fe
Nicolò Vescera <slayercandy@icloud.com>
2015-02-20 09:52:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -16,29 +16,23 @@ int main(int argc, char** argv) {
     scanf("%d",&(array[i]));
     }
     media=0;
-    for(i=0;i<N;i++){
-        media+=array[i];
-    }
-    media/=N;
-    
     massima=INT_MIN;
-    for(i=0;i<N;i++){
-        if(array[i]>massima)
-            massima=array[i];
-    }
-    
     minima=INT_MAX;
-    for(i=0;i<N;i++){
-        if(array[i]<minima)
-            minima=array[i];
-    }
-    
     capo=0;
     for(i=0;i<N;i++){
+        media+=array[i];
+        if(array[i]>massima)
+            massima=array[i];
+        if(array[i]<minima)
+            minima=array[i];
         if(array[i]>=REPUBBLICA)
             capo++;
     }
-  
+    media/=N;
+
+
+
+
     printf("Età media: %f\n",media);
     printf("Età massima: %d\n",massima);
     printf("Età minima: %d\n",minima);
